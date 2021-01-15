c78fa4f16650c232bfe7c4100a75af2585aa298d
Itunu Raimi <chavitunu@gmail.com>
2021-01-15 20:48:50 +0100

    setup project

---
+++ Bulls Eye Game/Bulls Eye Game/Bulls_Eye_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bulls_Eye_GameApp.swift
+//  Bulls Eye Game
+//
+//  Created by mac on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bulls_Eye_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
